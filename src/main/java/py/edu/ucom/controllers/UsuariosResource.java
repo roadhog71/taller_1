6d83beab94f2e5607642e945f6fa4fe4c44a43a6
Seb <63566437+roadhog71@users.noreply.github.com>
2023-10-18 16:47:25 -0300

    rebuild for final proyect branch

--- src/main/java/py/edu/ucom/controllers/UsuariosResource.java
+++ src/main/java/py/edu/ucom/controllers/UsuariosResource.java
@@ -7,26 +7,25 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
-import py.edu.ucom.entities.Usuarios;
-import py.edu.ucom.utils.DataSourceJSON;
+
 
 @Path("/usuarios")
 public class UsuariosResource {
-    @Inject
-    public DataSourceJSON ds;
+    // @Inject
+    // public DataSourceJSON ds;
 
-    @GET
-    public List<Usuarios> obtener(){
-        return this.ds.obtenerUsuarios();
-    }
-    @GET
-    @Path("{documento}")
-    public Usuarios obtenerByDocumento(@PathParam("documento")String param){
-        return this.ds.buscarUsuario(param);
-    }
+    // @GET
+    // public List<Usuarios> obtener(){
+    //     return this.ds.obtenerUsuarios();
+    // }
+    // @GET
+    // @Path("{documento}")
+    // public Usuarios obtenerByDocumento(@PathParam("documento")String param){
+    //     return this.ds.buscarUsuario(param);
+    // }
 
     @POST
-    public void guardar(Usuarios usuario){
+    public void guardar(Object usuario){
         this.ds.guardarUsuarios(usuario);
     }
     
